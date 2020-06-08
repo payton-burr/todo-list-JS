4a7ccba13389e568e34d125fe4efacb701f10faa
Payton Burr <36023709+payton-burr@users.noreply.github.com>
2020-06-08 17:46:44 -0600

    added filter function to sort todo items

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
+
 // Event Listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
-// Functions
+filterOption.addEventListener('change', filterTodo);
 
+// Functions
 function addTodo(e) {
   // Prevent form from submitting / page refresh
   e.preventDefault();
@@ -63,4 +66,29 @@ function deleteCheck(e) {
     const todo = item.parentElement;
     todo.classList.toggle('completed');
   }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case 'all':
+        todo.style.display = 'flex';
+        break;
+      case 'complete':
+        if (todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+        break;
+      case 'incomplete':
+        if (!todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+        break;
+    }
+  });
 }
