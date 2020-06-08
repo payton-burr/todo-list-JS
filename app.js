1f449eb2db2cc79c9d6b44c58db394028ff7a428
Payton Burr <36023709+payton-burr@users.noreply.github.com>
2020-06-08 15:53:03 -0600

    generating todos

---
+++ app.js
@@ -0,0 +1,40 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+// Event Listeners
+todoButton.addEventListener('click', addTodo);
+// Functions
+
+function addTodo(e) {
+  // Prevent form from submitting / page refresh
+  e.preventDefault();
+
+  // Todo Div
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+
+  // add Li
+  const newTodo = document.createElement('li');
+  newTodo.innerText = 'hello';
+  newTodo.classList.add('todo-item');
+
+  todoDiv.appendChild(newTodo);
+
+  // Checked button
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add('complete-btn');
+
+  todoDiv.appendChild(completedButton);
+
+  // trash button
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add('trash-btn');
+
+  todoDiv.appendChild(trashButton);
+
+  // Append to UL
+  todoList.appendChild(todoDiv);
+}
